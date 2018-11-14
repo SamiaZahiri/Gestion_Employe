cd533cbff71f65a42a29357eae61ffc6b60ddf8a
moncef ouaquasse <pandaeca@gmail.com>
2018-11-14 22:37:50 +0100

    Commited2

--- src/controller/FXMLDocumentController.java
+++ src/controller/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gestion_employe;
+package controller;
 
 import classe.Employee;
 import java.net.URL;
